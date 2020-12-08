c3a6238e7078be37347703dc19853c9b96a324bc
thomas LUNETEAU <thb8@outlook.fr>
2020-12-08 13:08:42 +0100

    critic & potion gérer
    Ajout changeStat, gestion retrait stat pot dans fight, gestion crit

--- Code/projet1_1/Fight.java
+++ Code/projet1_1/Fight.java
@@ -1,29 +1,49 @@
 import Characters.*;
+import Characters.Character;
+
 import java.util.HashMap;
 
 public class Fight {
 	private Player player;
 	private Enemy enemy;
 	private HashMap<String, Enemy> enemies = new HashMap<String, Enemy>();
-	private boolean usedAttackPotion;
-	private boolean usedDefensePotion;
-	private boolean usedCritPotion;
-
 	private int damage;
 	private boolean hasDefend = false;
-	private int playerDef = player.getDefense();
-
+	private int randomMax = 100;
+	private int coeffCrit = 2;
+	
 	public void instantiateFight(Player player){
 		player = this.player;
 		enemies = this.player.getMapHero().getEnemy() ;
 	}
+
+
+	private Player savePlayerBaseStat = player;
+	private int playerDef = player.getDefense();
 	
+
+
+	public int attackCrit(Character attacker){
+		int random100 = (int)(Math.random() * randomMax);
+
+		if(attacker.getCrit()<random100){
+			System.out.println("CRITIC !!!!!!!!!!\n")
+			return attacker.getAttack()*coeffCrit;
+		}
+		else{
+			return attacker.getAttack();
+		}
+	}
+
 
 	public int stillFighting(){
 		if (player.getHealth()<1){
 			return 1;
 		}
 		if(enemies.size()==0){		
+			player.getStatistics().changeAttack(savePlayerBaseStat.getAttack());
+			player.getStatistics().changeDefense(savePlayerBaseStat.getDefense());
+			player.getStatistics().changeCritical(savePlayerBaseStat.getCrit());
 			return 2;
 		}
 		return 0;
@@ -33,7 +53,7 @@ public class Fight {
 		
 		hasDefend = false;
 		System.out.println("You are attacking " + enemies.get(targetName).getName());
-		damage = player.getAttack()-enemies.get(targetName).getDefense();
+		damage = attackCrit(player)-enemies.get(targetName).getDefense();
 		if (damage>0){
 			enemies.get(targetName).getStatistics().removeHealth(damage);
 			System.out.println("You inflicted " + damage +" dmg. " + enemies.get(targetName).getName() +" has " + enemies.get(targetName).getHealth() + " HP left.\n");
@@ -52,7 +72,7 @@ public class Fight {
 	public void enemyAttack() {
 		for (String i : enemies.keySet()) {
 			System.out.println(enemies.get(i).getName() + " is attacking.\n");
-			damage = enemies.get(i).getAttack()-playerDef;
+			damage = attackCrit(enemies.get(i))-playerDef;
 			if (damage>0){
 				player.getStatistics().removeHealth(damage);
 				System.out.println(enemies.get(i).getName() + " has inflicted " + damage +" dmg, you have " + player.getHealth() + " HP remaining.\n");
--- Code/projet1_1/Stats/Statistics.java
+++ Code/projet1_1/Stats/Statistics.java
@@ -99,6 +99,10 @@ public abstract class Statistics {
 
 	}
 
+	public void changeHealth(int hp){
+		this.health = hp;
+	}
+
 	/**
 	 * 
 	 * @param def
@@ -106,6 +110,9 @@ public abstract class Statistics {
 	public void removeDefense(int def) {
 
 		this.defense -= def;
+	}
+	public void changeDefense(int def){
+		this.defense = def;
 	}
 
 	/**
@@ -116,6 +123,9 @@ public abstract class Statistics {
 		
 		this.critical -= crit;
 	}
+	public void changeCritical(int crit){
+		this.critical = crit;
+	}
 
 	/**
 	 * 
@@ -124,6 +134,9 @@ public abstract class Statistics {
 	public void removeAttack(int att) {
 		
 		this.attack -= att;
+	}
+	public void changeAttack(int att){
+		this.attack = att;
 	}
 
 	/**
