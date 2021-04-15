849d89576c8c66de2da31a918d60d470595e9dd1
thomas LUNETEAU <thb8@outlook.fr>
2021-04-15 16:28:31 +0200

    ajouts ids figths

--- SpaceandPens/src/Controllers/FightController.java
+++ SpaceandPens/src/Controllers/FightController.java
@@ -9,6 +9,15 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 /**
@@ -19,7 +28,57 @@ import javafx.scene.layout.VBox;
 public class FightController implements Initializable {
 
     @FXML
-    private VBox vbox_hp_player;
+    private VBox World;
+    @FXML
+    private MenuBar menubar;
+    @FXML
+    private Menu option;
+    @FXML
+    private MenuItem help;
+    @FXML
+    private MenuItem quit;
+    @FXML
+    private AnchorPane fightPanel;
+    @FXML
+    private VBox playerDisplay;
+    @FXML
+    private ImageView playerPicture;
+    @FXML
+    private VBox vboxPlayer;
+    @FXML
+    private Label labelNamePlayer;
+    @FXML
+    private Label maxHPPLAYER;
+    @FXML
+    private Label playerHP;
+    @FXML
+    private ProgressBar HPBarPlayer;
+    @FXML
+    private Label attackPlayer;
+    @FXML
+    private Label PlayerDefense;
+    @FXML
+    private VBox enemuDisplay;
+    @FXML
+    private ImageView enemyPicture;
+    @FXML
+    private VBox vboxEnemy;
+    @FXML
+    private Label labelNameEnemy;
+    @FXML
+    private ProgressBar HPBarEnemy;
+    @FXML
+    private Label labelAction;
+    @FXML
+    private Label description;
+    @FXML
+    private GridPane choices;
+    @FXML
+    private Button attackButton;
+    @FXML
+    private Button defendButton;
+    @FXML
+    private Button InventoryButton;
 
     /**
      * Initializes the controller class.
